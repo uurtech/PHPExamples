fc8b34c02a512cea1fbe26b7171f05c2404ec865
Ugur KAZDAL <ugur.kazdal@1ki3.com>
2016-12-09 23:52:49 +0300

    Bozuk para hesabı
    scripte başlandı

---
+++ bozukParaHesabi.php
@@ -0,0 +1,17 @@
+<?php
+
+//ödenecek hesap random verilecek (float/double şeklinde)
+$ucret = rand(100,1000) / 10;
+
+//100 ile 1000 arasında bir sayı alıp 10'a bölüyoruz ki, 150.7 gibi bir rakam gelebilsin.
+//100 ile 1000 arasında bir rakam veri 100'e bölersek 15.07 gibi bir rakam alırız.
+
+echo "Lütfen cebinizdeki parayı giriniz :) ";
+$alinanUcret = (double)trim(fgets(STDIN));
+
+if(($alinanUcret - $ucret) < 0){
+	echo "Paranız yetmiyor !!!";
+	exit;
+}
+
+?>
